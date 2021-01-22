0760e801791bdabab0e77e542b814bdad4fda9db
ivanjiang <79993067@qq.com>
2021-01-22 19:35:27 +0800

    update

--- src/main/scala/com/ivan/spark/ch4/Demo.scala
+++ src/main/scala/com/ivan/spark/ch4/Demo.scala
@@ -3,7 +3,7 @@ package com.ivan.spark.ch4
 object Demo {
   def main(args: Array[String]): Unit = {
     val list1 = List(1, 2, 3)
-    val list2 = list1.map(_ + 1);
+    val list2 = list1.map(_ + 1)
     println(list2)
   }
 }
