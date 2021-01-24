8b841fc4bf9b4576405137ff7f839899961f4bc4
ivanjiang <79993067@qq.com>
2021-01-25 01:18:44 +0800

    update

--- src/main/scala/com/ivan/spark/ch4/RDF.scala
+++ src/main/scala/com/ivan/spark/ch4/RDF.scala
@@ -1,13 +1,15 @@
 package com.ivan.spark.ch4
 
-import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.classification.{DecisionTreeClassifier, RandomForestClassificationModel, RandomForestClassifier}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.feature.{VectorAssembler, VectorIndexer}
+import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
 import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.DoubleType
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 import scala.util.Random
 
@@ -42,9 +44,12 @@ object RDF {
     testData.cache()
     //trainData.select("Elevation", "Cover_Type").show(100)
     val runRDF = new RunRDF(spark)
-    runRDF.simpleDecisionTree(trainData, testData)
-    runRDF.randomClassifier(trainData, testData)
-    runRDF.evaluate(trainData, testData)
+    //runRDF.simpleDecisionTree(trainData, testData)
+    //runRDF.randomClassifier(trainData, testData)
+    //runRDF.evaluate(trainData, testData)
+    //runRDF.evaluateCategorical(trainData, testData)
+    runRDF.evaluateForest(trainData, testData)
+
     trainData.unpersist()
     testData.unpersist()
   }
@@ -53,6 +58,77 @@ object RDF {
 class RunRDF(private val spark: SparkSession) {
 
   import spark.implicits._
+
+  def unencodeOneHot(data: DataFrame) = {
+    val wildernessCols: Array[String] = (0 until 4).map(i => s"Wilderness_Area_$i").toArray
+    val wildernessAssembler = new VectorAssembler()
+      .setInputCols(wildernessCols)
+      .setOutputCol("wilderness")
+    val unhotUDF = udf((vec: Vector) => vec.toArray.indexOf(1.0).toDouble)
+    val withWilderness = wildernessAssembler.transform(data)
+      .drop(wildernessCols: _*)
+      .withColumn("wilderness", unhotUDF($"wilderness"))
+
+    val soilCols = (0 until 40).map(i => s"Soil_Type_$i").toArray
+    val soliAssembler = new VectorAssembler()
+      .setInputCols(soilCols)
+      .setOutputCol("soil")
+    soliAssembler.transform(withWilderness)
+      .drop(soilCols: _*)
+      .withColumn("soil", unhotUDF($"soil"))
+  }
+
+  def evaluateCategorical(trainData: Dataset[Row], testData: Dataset[Row]) = {
+    val unencTrainData = unencodeOneHot(trainData)
+    println("unencTrainData_")
+    unencTrainData.printSchema()
+    unencTrainData.show(5, truncate = false)
+    val unencTestData = unencodeOneHot(testData)
+    val inputCols = unencTrainData.columns.filter(_ != "Cover_Type")
+    val assembler = new VectorAssembler()
+      .setInputCols(inputCols)
+      .setOutputCol("featureVector")
+
+    val indexer = new VectorIndexer()
+      .setMaxCategories(40)
+      .setInputCol("featureVector")
+      .setOutputCol("indexedVector")
+
+    val classifier = new DecisionTreeClassifier()
+      .setSeed(Random.nextLong())
+      .setLabelCol("Cover_Type")
+      .setFeaturesCol("indexedVector")
+      .setPredictionCol("prediction")
+
+    val pipeline = new Pipeline().setStages(Array(assembler, indexer, classifier))
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(classifier.impurity, Seq("gini", "entropy"))
+      .addGrid(classifier.maxDepth, Seq(1, 20))
+      .addGrid(classifier.maxBins, Seq(40, 300))
+      .addGrid(classifier.minInfoGain, Seq(0.0, 0.05))
+      .build()
+
+    val eval = new MulticlassClassificationEvaluator()
+      .setMetricName("accuracy")
+      .setPredictionCol("prediction")
+      .setLabelCol("Cover_Type")
+
+    val validator = new TrainValidationSplit()
+      .setSeed(Random.nextLong())
+      .setEvaluator(eval)
+      .setEstimatorParamMaps(paramGrid)
+      .setEstimator(pipeline)
+      .setTrainRatio(0.9)
+
+    val validatorModel = validator.fit(unencTrainData)
+    val bestModel = validatorModel.bestModel
+    println(bestModel.asInstanceOf[PipelineModel].stages.last.extractParamMap())
+
+    val trainAccuracy = eval.evaluate(bestModel.transform(unencTrainData))
+    println("trainAccuracy " + trainAccuracy)
+    val testAccuracy = eval.evaluate(bestModel.transform(unencTestData))
+    println("testAccuracy " + testAccuracy)
+  }
 
   def simpleDecisionTree(trainData: DataFrame, testData: DataFrame): Unit = {
     val inputCols = trainData.columns.filter(_ != "Cover_Type")
@@ -174,5 +250,70 @@ class RunRDF(private val spark: SparkSession) {
     val testAccuracy = multiclassEval.evaluate(predictionsForTestData)
     val trainAccuracy = multiclassEval.evaluate(predictionsForTrainData)
     println("trainAccuracy " + trainAccuracy + ", testAccuracy" + testAccuracy)
+  }
+
+  def evaluateForest(trainData: DataFrame, testData: DataFrame): Unit = {
+    val unencTrainData = unencodeOneHot(trainData)
+    println("unencTrainData_")
+    unencTrainData.printSchema()
+    unencTrainData.show(5, truncate = false)
+    val unencTestData = unencodeOneHot(testData)
+    val inputCols = unencTrainData.columns.filter(_ != "Cover_Type")
+    val assembler = new VectorAssembler()
+      .setInputCols(inputCols)
+      .setOutputCol("featureVector")
+
+    val indexer = new VectorIndexer()
+      .setMaxCategories(40)
+      .setInputCol("featureVector")
+      .setOutputCol("indexedVector")
+
+    val classifier = new RandomForestClassifier()
+      .setSeed(Random.nextLong())
+      .setLabelCol("Cover_Type")
+      .setFeaturesCol("indexedVector")
+      .setPredictionCol("prediction")
+      .setImpurity("entropy")
+      .setMaxDepth(20)
+      .setMaxBins(300)
+
+    val pipeline = new Pipeline().setStages(Array(assembler, indexer, classifier))
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(classifier.minInfoGain, Seq(0.0, 0.05))
+      .addGrid(classifier.numTrees, Seq(1, 10))
+      .build()
+
+    val eval = new MulticlassClassificationEvaluator()
+      .setMetricName("accuracy")
+      .setPredictionCol("prediction")
+      .setLabelCol("Cover_Type")
+
+    val validator = new TrainValidationSplit()
+      .setSeed(Random.nextLong())
+      .setEvaluator(eval)
+      .setEstimatorParamMaps(paramGrid)
+      .setEstimator(pipeline)
+      .setTrainRatio(0.9)
+
+    val validatorModel = validator.fit(unencTrainData)
+    val bestModel = validatorModel.bestModel
+    println(bestModel.asInstanceOf[PipelineModel].stages.last.extractParamMap())
+
+    val forestModel = bestModel.asInstanceOf[PipelineModel]
+      .stages.last.asInstanceOf[RandomForestClassificationModel]
+
+    println(forestModel.extractParamMap())
+    println(forestModel.getNumTrees)
+
+    forestModel.featureImportances.toArray.zip(inputCols)
+      .sorted.reverse.foreach(println)
+
+    val trainAccuracy = eval.evaluate(bestModel.transform(unencTrainData))
+    println("trainAccuracy " + trainAccuracy)
+    val testAccuracy = eval.evaluate(bestModel.transform(unencTestData))
+    println("testAccuracy " + testAccuracy)
+
+    bestModel.transform(unencTestData.drop("Cover_Type"))
+      .select("prediction").show()
   }
 }
